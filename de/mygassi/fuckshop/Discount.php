0e68e77e19b86c0540e1f47002fb8275d850099c
viktorberzsinszky <vberzsin@gmail.com>
2014-03-17 17:16:29 +0100

    discountär

--- de/mygassi/fuckshop/Discount.php
+++ de/mygassi/fuckshop/Discount.php
@@ -14,9 +14,25 @@ class Discount
 		$this->rules[]= $rule;
 	}
 
+	/*
+	This is ugly.
+	An order consists of products, taxes, discount whatsoever.
+	The order (and the products whithin thit might apply a given discount-rule, or | even an coupon
+		sets up an invoice
+		that gets presented
+			that "freezes" 
+			and than gets payed
+		"clazz Discount" is bull.
+		The Order applies the given discount rules (as for products or for the cart or for whatever reason
+			and generates 
+				the invoice
+				the packaging
+				...
+		fuckdiss discount	
+	*/
 	public function apply($amount)
 	{
-		$res = -100;
+		$res = 0;
 		foreach($this->rules as $rule){
 			switch($rule->type){
 				case "presserabatt":
