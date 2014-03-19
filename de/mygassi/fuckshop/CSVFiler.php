5c78cd55e99baa92863d6c2a0a1d3bfb66221c18
viktorberzsinszky <vberzsin@gmail.com>
2014-03-19 13:13:10 +0100

    adminpanel

---
+++ de/mygassi/fuckshop/CSVFiler.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+
+ Sales-Fraggles like their data in Excel anyway
+	The import.
+	The export.
+ 
+ There is no way they are going to click through an "Adminpanel".
+ And than this is good in fact.
+
+ */
+
+class CSVFiler
+{
+
+}
