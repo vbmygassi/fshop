3904d2289af66407edca33da10ddd834d94291d5
viktorberzsinszky <vberzsin@gmail.com>
2014-03-19 14:03:29 +0100

    adminpanel

--- de/mygassi/fuckshop/CSVFiler.php
+++ de/mygassi/fuckshop/CSVFiler.php
@@ -2,15 +2,18 @@
 
 /*
 
- Salesfraggles like their data in the excel way
-	So goes the import.
-	As goes alogn the export.
-	As go the the sales reports.
+ Salesfraggles use excel and no admin panels. 
+ Excel is wrong: no import or such. 
+	Excel it again plz.
 
- Nobody, i really mean NOBODY is going to click the Adminpanel. 
+ Import is from Excel CSV.
+ Export is to Excel CSV.
+
+ Nobody, i really mean NOBODY is going to click an "Adminpanel". 
  And especially NOT YOU.
  And than this is good in fact.
  
+ [ Adminpanels usually start with a "user: pass" combination and groups and policies and access control lists and such and i really do not think adminpanels make us happy
 
  */
 
